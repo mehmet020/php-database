356849c1ee345803afa743ea2fded072a4ce24d2
mehmet020 <131659237+mehmet020@users.noreply.github.com>
2023-12-01 11:30:00 +0100

    Add files via upload

--- db.php
+++ db.php
@@ -1,16 +1,31 @@
 <?php
-
 class Database {
-    public $pdo;
+    private $pdo;
+    public function __construct($host, $dbname, $username, $password) {
+        $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
 
-    public function __construct($db = "sys", $user="root", $pwd="M@hmet2005", $host="localhost"){
-        try{
-            $this->pdo = new PDO ( "mysql: host=$host ; dbname=$db", $user, $pwd);
-            $this->pdo->setattribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "connected to Database $db"; 
-        }catch (PDOExecption $e){
-            echo "connection failed" . $e->getMessage();
+    public function insertData($column1, $column2, $column3) {
+        try {
+            $sql = "INSERT INTO jouw_tabel (kolom1, kolom2, kolom3) VALUES (:column1, :column2, :column3)";
+
+            $stmt = $this->pdo->prepare($sql);
+
+            $stmt->bindParam(':column1', $column1);
+            $stmt->bindParam(':column2', $column2);
+            $stmt->bindParam(':column3', $column3);
+
+            $stmt->execute();
+
+            echo "Data succesvol toegevoegd aan de tabel.";
+        } catch (PDOException $e) {
+            echo "Fout bij het toevoegen van data: " . $e->getMessage();
         }
     }
 }
+
+$database = new Database('jouw_host', 'jouw_database', 'jouw_gebruikersnaam', 'jouw_wachtwoord');
+$database->insertData('waarde1', 'waarde2', 'waarde3');
+
 ?>
